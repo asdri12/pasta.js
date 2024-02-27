abbdbeac8a8bf16b27804f2c4d0143791e2c391c
asdri12 <128160229+asdri12@users.noreply.github.com>
2024-02-27 10:36:29 -0300

    Add files via upload

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const text1 = "ola, mundo!";
+const text2 = 'ola, mundo!';
+const senha = "senhasupersegura456";
+const stringDenumero = "34567";
+
+const citacao = "meu nome e ";
+const meunome = "samara"
+
+// concatenacao (+)
+
+console.log(citacao + meunome)
+
+// template string OU template literal
